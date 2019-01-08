47ec68ba89e75302ab93f2b44de8d6e5ec1c6671
Sharad Katre <sharad.katre@mobisoftinfotech.com>
2019-01-08 11:30:12 +0530

    Initial Commit

---
+++ SplashTutorialDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashTutorialDemo
+//
+//  Created by Sharad Katre on 08/01/19.
+//  Copyright © 2019 Sharad Katre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
